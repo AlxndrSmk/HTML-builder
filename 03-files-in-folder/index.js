1e763efdd6d5b8514263cd82e4291ba97aa4504d
Alexander Samak <voprosys262@gmail.com>
2023-05-05 19:35:16 +0300

    fix: extention splitting

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,8 +9,8 @@ fsp.readdir(directory, { withFileTypes: true }).then((files) => {
     if (file.isFile()) {
       const filePath = path.resolve(__dirname, 'secret-folder', file.name);
       fsp.stat(filePath).then((stat) => {
-        const fileName = path.basename(filePath).split('.')[0];
         const fileExtention = path.extname(filePath).slice(1);
+        const fileName = path.basename(filePath).split(`.${fileExtention}`)[0];
         const fileSize = Math.round((stat.size / 1024) * 1000) / 1000;
 
         console.log(`${fileName} - ${fileExtention} - ${fileSize}kb`);
